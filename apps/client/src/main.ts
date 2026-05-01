09fa1e11c3bab006dc6b948a8e593a043ee28379
anguiao <anguiao@live.cn>
2026-05-01 17:54:36 +0800

    chore: add auth frontend dependencies

--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,3 +1,4 @@
+import { PiniaColada } from '@pinia/colada'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -5,8 +6,10 @@ import { router } from './router'
 import './style.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
+app.use(PiniaColada)
 app.use(router)
 
 app.mount('#app')

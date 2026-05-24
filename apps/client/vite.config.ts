0b7cd35c3a962010b1f467dadcb708279b06caba
anguiao <anguiao@live.cn>
2026-05-24 22:21:23 +0800

    chore: add Vue DevTools

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,16 +1,18 @@
 import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
-import VueRouter from 'vue-router/vite'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import vueRouter from 'vue-router/vite'
 import { getFileBasedRouteName } from 'vue-router/unplugin'
 
 export default defineConfig({
   plugins: [
-    VueRouter({
+    vueRouter({
       watch: process.env.NODE_ENV === 'development' && !process.env.CI,
       getRouteName: (routeNode) => getFileBasedRouteName(routeNode).replace(/\/+/g, '/'),
     }),
     vue(),
+    vueDevTools(),
     tailwindcss(),
   ],
   server: {

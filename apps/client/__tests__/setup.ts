b9044cc33aa94e71c8c3c3eedb0b7c7afd2a9a32
anguiao <anguiao@live.cn>
2026-05-25 19:46:55 +0800

    test: mock iconify in client tests

--- apps/client/__tests__/setup.ts
+++ apps/client/__tests__/setup.ts
@@ -1,5 +1,33 @@
-import { afterEach } from 'vitest'
+import { afterEach, vi } from 'vitest'
 import { disposeActiveTestPinia } from './helpers/pinia'
+
+vi.mock('@iconify/vue', async () => {
+  const { defineComponent, h } = await vi.importActual<typeof import('vue')>('vue')
+
+  return {
+    Icon: defineComponent({
+      name: 'Icon',
+      props: {
+        icon: {
+          type: String,
+          required: true,
+        },
+      },
+      setup(props, { attrs }) {
+        return () =>
+          h(
+            'span',
+            {
+              ...attrs,
+              'data-test': 'iconify-icon',
+              'aria-hidden': 'true',
+            },
+            props.icon,
+          )
+      },
+    }),
+  }
+})
 
 afterEach(() => {
   disposeActiveTestPinia()
